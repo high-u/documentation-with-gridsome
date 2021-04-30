58e12c4aa6db0797d2cc852dcb975c975ae558a0
high-u <zen.crazyd@gmail.com>
2021-04-30 09:53:01 +0900

    Add search result highlight

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -6,7 +6,14 @@
       <article v-for="post in searchResults" :key="post.node.id">
         <h2><g-link :to="post.node.path">{{ post.node.title }}</g-link></h2>
         <p>{{ post.node.date }}</p>
-        <p>{{ post.node.summary }}</p>
+
+        <span
+          v-for="(words, index) in post.node.highlightSummary" :key="index"
+          :class="{'highlight': words == search}"
+          >
+          {{ words }}
+        </span>
+
       </article>
     </div>
 
@@ -41,11 +48,29 @@ export default {
   },
   computed: {
     searchResults() {
-      return this.$static.allPost.edges.filter(post => {
+      const hits = this.$static.allPost.edges.filter(post => {
         return post.node.summary.toLowerCase().includes(this.search.toLowerCase().trim())
       }).slice(0, 3)
+      console.log(hits)
+      if (this.search.length > 0) {
+        return hits.map(e => {
+          e.node.highlightSummary = e.node.summary.replaceAll(this.search, `\u{000b}${this.search}\u{000b}`).split("\u{000b}")
+          return e
+        })
+      } else {
+        return hits.map(e => {
+          e.node.highlightSummary = [e.node.summary]
+          return e
+        })
+      }
     }
   }
 }
 </script>
 
+<style>
+.highlight {
+  color: white;
+  background-color: black;
+}
+</style>
