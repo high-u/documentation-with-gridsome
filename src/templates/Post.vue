507244cea0fb57f9bb484f6f5c21b03084d26aee
high-u <zen.crazyd@gmail.com>
2021-05-01 14:29:52 +0900

    add markdown style

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -16,7 +16,7 @@
 
       <p>Posted on {{ $page.post.date }}</p>
 
-      <div class="markdown-body mb-8" id="article-area" v-html="$page.post.content" />
+      <div class="markdown-body mb-8" id="article-area" v-html="contentWithTailwind" />
     </article>
   </Layout>
 </template>
@@ -48,6 +48,27 @@ query Post ($path: String!) {
   computed: {
     filepath: function() {
       return `https://github.com/high-u/documentation-with-gridsome/blob/main/${this.$page.post.fileInfo.path}`
+    },
+    contentWithTailwind() {
+      let domparser = new DOMParser()
+      let doc = domparser.parseFromString(this.$page.post.content, "text/html")
+      // console.log(doc)
+      // doc.querySelectorAll(".xxx")
+      // let pres = doc.getElementsByTagName("pre")
+      // console.log(pres)
+      // let button = document.createElement("button")
+      // let appendedButton = pres[0].appendChild(button)
+      // console.log("pres[0]", pres[0])
+      // console.log("appendedButton", appendedButton)
+
+      let h2 = doc.getElementsByTagName("h2")
+      for (let e of h2) {
+        e.classList.add("text-yellow-500", "underline")
+      }
+
+
+      // console.log(doc.documentElement.innerHTML)
+      return doc.documentElement.innerHTML
     }
   }
 }
